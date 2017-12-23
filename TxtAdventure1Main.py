0402f0f2231ac3cfb443049384a5886ae596fdb7
Gmoney2123 <gjgaddy15@gmail.com>
2017-12-23 16:23:58 -0600

    implimented logo
    implimented the logo and tweeked the comments here and there.
    
    Also fixed a goofy thing I did where I used `input()` to print something to the screen and then assigned it to a variable? probably did this tho keep the terminal from closing.

--- TxtAdventure1Main.py
+++ TxtAdventure1Main.py
@@ -1,16 +1,15 @@
 # Information:
-# Author: Garrett Gaddy
 # Program type: Text-adventure Game
 # Project start date: 9-20-2017
 
 
 ### notes about my code ###
 # All variables that are used specifically for lists will be suffixed with '_lst'
-#For example Fruit_lst = ['apple', 'orange', 'grape']
+# For example Fruit_lst = ['apple', 'orange', 'grape']
 
-#I use lots of external .txt files to draw descriptors, names, etc.
-#you can find these files in the 'resources' folder.
-#each set of lists is clearly named and in clearly marked (I hope...) folders.
+# I use lots of external .txt files to draw descriptors, names, etc.
+# you can find these files in the 'Resources' folder.
+# each set of lists is clearly named and in clearly marked folders. (I hope...)
 
 ### end of notes ###
 
@@ -20,11 +19,11 @@ import os
 import shutil
 import sys
 
-#Detects if the user's input for their name includes a number.
+# Detects if the user's input for their name includes a number.
 def has_numbers(inputString):
     return any (char.isdigit() for char in inputString)
 
-#initializes character and location classes
+# initializes character and location classes
 class Character():
     def __init__(self):
         self.name = ""
@@ -37,43 +36,46 @@ class LocationNames():
         self.type = ""
         self.weather = ""
 
-#simplifies text alignment
+# simplifies text alignment
 def align_center(s):
     return s.center(shutil.get_terminal_size().columns)
 
-#this function clears the screen when nesseary.
-def cls():
+# this function clears the screen when nesseary.
+def clear():
     if sys.platform == "win32":
         os.system('cls')
     else:
         os.system('clear')
-        
-#links the classes "Character" and "LocationNames" to enable variable instances.
+
+# links the classes "Character" and "LocationNames" to enable variable instances.
 User = Character()
 MainCity = LocationNames()
 
-#randomly selects and assigns a weather adjective for an associated city.
+# randomly selects and assigns a weather adjective for an associated city.
 weather_lst = open("Resources/Weather/weatherMain.txt").read().splitlines()
 MainCity.weather = random.choice(weather_lst)
 
-#randomly selects a city name from the .txt file and assigns it.
+# randomly selects a city name from the .txt file and assigns it.
 cities_lst = open("Resources/Cities&Towns/city_names.txt").read().splitlines()
 MainCity.name = random.choice(cities_lst)
 
-#classifies the established settlement as a "town", "village", "city", etc.
+# classifies the established settlement as a "town", "village", "city", etc.
 city_class_lst = open("Resources/Cities&Towns/city_type.txt").read().splitlines()
 MainCity.type = random.choice(city_class_lst)
 
-#randomized gatekeeper name
+# randomized gatekeeper name
 gatekeeper_name_lst = open("Resources/characters/gatekeepers.txt").read().splitlines()
 gatekeeper_name = random.choice(gatekeeper_name_lst)
 
-#define other variables
+# load the logo and display when called as a variable
+logo = open("Resources/logos/logo_main.txt").read()
+
+# define other variables
 start_check = ""
 check_quit = ""
 error = "Invalid input, Please try again."
 
-#check if the user would like to start the game.
+# check if the user would like to start the game.
 while start_check is "":
     check_quit = ""
     start_check = input("Would you like to begin? (Yes or No): ")
@@ -82,7 +84,7 @@ while start_check is "":
         start_check = ""
     elif start_check in {'YES','Yes','yes','y'}:
         break
-    #if no, verify with 'are you sure?' before terminating.
+#   if no, verify with 'are you sure?' before terminating.
     elif start_check in {'no','NO','No','n'}:
         while check_quit is "":
             check_quit = input("Are you sure you wish to quit? (Yes or No): ")
@@ -91,34 +93,42 @@ while start_check is "":
                 check_quit = ""
             elif check_quit in ['YES','Yes','yes','y']:
                 quit()
-    #if they decide they DON'T want to quit, remain in the loop by setting "start_check" back to the "{empty}" state.
+#    if they decide they DON'T want to quit, remain in the loop by setting "start_check" back to the "{empty}" state.
             elif check_quit in ['no','NO','No','n']:
                 start_check = ""
                 break
 
-#I want this to be replaced with a script segment
-#that randomly generates a description of the weather, the town, and details of the town.
-#Will draw from .txt files and randomize to variables in classes for infinite combinations.
+# when beginging the game display the logo
+print("Welcome, to... \n" + logo)
+time.sleep(5)
+clear()
+
+# I want the following to be replaced with a script segment
+# that randomly generates a description of the weather, the town, and details of the town.
+# Will draw from .txt files and randomize to variables in classes for infinite combinations.
 print(align_center("You slowly and exhaustedly crest the hill, a " + MainCity.type + " appearing on the horizon."))
 time.sleep(3)
-print(align_center("You know this place infront of you... it\'s " + MainCity.name + "." ))
+print(align_center("You know the place in front of you... it\'s " + MainCity.name + "." ))
 
-#need to create a variable in which the value changes based on the weather. I.E. it wouldnt make sense to take shelter on a sunny day.      
+# need to create a variable in which the value changes based on the weather.
+# I.E. it wouldnt make sense to take shelter on a sunny day.
 time.sleep(3)
 print(align_center("The weather is " + MainCity.weather + ", it might be a good idea to stop and rest."))
 time.sleep(3)
+
+# here, MainCity.weather should be changed to a new variable that would dislay a chracteristic of MainCity.weather
 print(align_center("As you approach you see a gate keeper come out to meet you in the " + MainCity.weather +" weather."))
 
 
 
-print (align_center("Well met traveler! I am " + gatekeeper_name +", I keep tabs on who comes and goes here."))
-time.sleep(1)
+print (align_center("Well met traveler! I am " + gatekeeper_name + ", I keep tabs on who comes and goes here."))
+time.sleep(2)
 while User.name == "":
     User.name = input("\n What is your name? (Enter name): ")
-    
+
     if has_numbers(User.name) == True:
         print ("That's not a valid name. Please don't use numbers.")
-        time.sleep(1)
+        time.sleep(2)
         User.name = ""
     elif has_numbers(User.name) == False:
         print (align_center("Ah! Fair travels " + User.name + "!"))
@@ -137,14 +147,14 @@ while User.gender == "":
         time.sleep(1)
 
 
-#   Generalizing multiple possible responses
+#   Generalizing multiple possible responses, and then contining story based on that response
     elif User.gender in {'boy', 'BOY', 'Boy'}:
         User.gender = "boy"
         time.sleep(1)
-        usr_action = input(align_center("Ah? Well, tread carefully around here Boy, the garrison is snapping up young lads by the ton around here..."))
+        print("Oh? Well, tread carefully around here Boy, the garrison is snapping up young lads by the ton...")
         break
     elif User.gender in {'girl', 'GIRL', 'Girl'}:
         User.gender = "girl"
         time.sleep(1)
-        usr_action = input(align_center("Oh? And what's a lady like yourself doin\' traveling alone?"))
+        print("Oh? And what's a lady like yourself doin\' traveling alone?")
         break
